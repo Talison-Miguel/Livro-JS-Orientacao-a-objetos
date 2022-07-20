cf89b50d0946a5bfa732ce69f05f68447d1a907d
Tálison Miguel <talisonmiguel84@gmail.com>
2022-07-20 20:21:25 -0300

    verificar existencia de uma propiedade

--- objetos.js
+++ objetos.js
@@ -5,3 +5,21 @@ let person1 = {
 
 person1.age = "18 anos"
 console.log(person1.name, person1.age)
+
+
+console.log("______________________")
+//Verificar se existe uma propiedade
+let person2 = {
+    name: "Talison",
+    sairnome: function () {
+        console.log(this.name)
+    }
+} 
+
+console.log("name" in person2)
+console.log("age" in person2)
+console.log("sairnome" in person2)
+
+//metodo hasOwnProperty() _ verifica se existe tal propiedade e retorna true se for propieade propia
+console.log("toString" in person1)
+console.log(person1.hasOwnProperty("toString"))
