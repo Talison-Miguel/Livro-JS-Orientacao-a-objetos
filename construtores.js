8058e70865fbdd5c484645a3463cf832effd041e
Tálison Miguel <talisonmiguel84@gmail.com>
2022-08-12 09:14:33 -0300

    constructors 1

--- construtores.js
+++ construtores.js
@@ -1,1 +1,12 @@
 //CONTRUTORES
+//é uma funçao usada com o operador new para criar um objeto
+//sempre começa com letra maiuscula
+
+function Person() {
+    //intencionalmente vazia
+}
+
+let perso1 = new Person();
+
+console.log(perso1 instanceof Person)
+console.log(perso1.constructor === Person)
