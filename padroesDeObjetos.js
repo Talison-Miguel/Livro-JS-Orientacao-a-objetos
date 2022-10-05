2132cf6cc7e2a48a35557deff52bfefc0c8477d5
Tálison Miguel <talisonmiguel84@gmail.com>
2022-10-05 17:17:06 -0300

    Membros privados de construtores

---
+++ padroesDeObjetos.js
@@ -0,0 +1,63 @@
+//Padrao de módulo
+
+//IIFE _ FUNÇAO CHAMADA IMEDIATAMENTE
+let yourObject = (function() {
+    //varaiveis refentes a dados privados
+    return {
+        //Metodos e propiedades publicas
+    }
+}())
+
+
+
+var person = (function() {
+    var age = 25
+
+    return {
+        name: "Talison",
+        getAge: function() {
+            return age
+        },
+        growOlder: function() {
+            age++
+        }
+    }
+})
+// console.log(person.name)
+// console.log(person.getAge())
+
+// person.age = 100
+// console.log(person.getAge())
+
+// person.growOlder()
+// console.log(person.getAge())
+
+
+console.log("____________________")
+
+
+//Membros privados de construtores
+function Person(name) {
+
+    //define uma variavel acessivel somente no construtor Person
+    var age = 25
+    this.name = name
+
+    this.getAge1 = function() {
+        return age
+    }
+
+    this.growOlder1 = function() {
+        age++
+    }
+}
+
+var person1 = new Person("Miguel")
+console.log(person.name)
+console.log(person.getAge1())
+
+person.age = 100
+console.log(person.getAge1())
+
+person.growOlder1()
+console.log(person.getAge1())
